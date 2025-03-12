2f2266137edc2ccade0459d40278b046a0e4f5d7
NatthanonPPP <natthanon.m2000@gmail.com>
2025-03-12 15:14:20 +0700

    PUT, Delete

--- learnFiber.go
+++ learnFiber.go
@@ -22,6 +22,9 @@ func main() {
 
 	app.Get("/books", getBooks)
 	app.Get("/books/:id", getBook)
+	app.Post("/books", createBook)
+	app.Put("/books/:id", updateBook)
+	app.Delete("/books/:id", deleteBook)
 
 	app.Listen(":8080")
 }
@@ -38,6 +41,48 @@ func getBook(c *fiber.Ctx) error {
 	for _, book := range books {
 		if book.ID == bookId {
 			return c.JSON(book)
+		}
+	}
+	return c.SendStatus(fiber.StatusNotFound)
+}
+
+func createBook(c *fiber.Ctx) error {
+	book := new(Book)
+	if err := c.BodyParser(book); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+	books = append(books, *book)
+	return c.JSON(book)
+}
+
+func updateBook(c *fiber.Ctx) error {
+	bookId, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+	bookUpdate := new(Book)
+	if err := c.BodyParser(bookUpdate); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+	for i, book := range books {
+		if bookId == book.ID {
+			books[i].Title = bookUpdate.Title
+			books[i].Author = bookUpdate.Author
+			return c.JSON(books[i])
+		}
+	}
+	return c.SendStatus(fiber.StatusNotFound)
+}
+
+func deleteBook(c *fiber.Ctx) error {
+	bookId, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+	for i, book := range books {
+		if bookId == book.ID {
+			books = append(books[:i], books[i+1:]...)
+			return c.SendStatus(fiber.StatusNoContent)
 		}
 	}
 	return c.SendStatus(fiber.StatusNotFound)
