2688e20e1751baab26a2945b63a3a770eb64cde2
NatthanonPPP <126769230+NatthanonPPP@users.noreply.github.com>
2025-03-19 12:59:39 +0700

    create database.go

--- GO-DATABASE/main.go
+++ GO-DATABASE/main.go
@@ -45,7 +45,7 @@ func main() {
 
 	fmt.Println("Connect Database Successful.")
 
-	// err = creatProduct(&Product{Name: "Go product 2", Price: 444})
+	// err = createProduct(&Product{Name: "Go product 2", Price: 444})
 	// if err != nil {
 	// 	log.Fatal(err)
 	// }
@@ -70,76 +70,5 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	fmt.Println(products)
-}
-
-func creatProduct(product *Product) error {
-	_, err := db.Exec(
-		"INSERT INTO public.products(name, price) VALUES ($1, $2);",
-		product.Name,
-		product.Price,
-	)
-	return err
-}
-
-func getProduct(id int) (Product, error) {
-	var p Product
-
-	row := db.QueryRow(
-		"SELECT id, name, price FROM products WHERE id=$1;",
-		id,
-	)
-
-	err := row.Scan(&p.ID, &p.Name, &p.Price)
-	if err != nil {
-		return Product{}, err
-	}
-
-	return p, nil
-}
-
-func getProducts() ([]Product, error) {
-	rows, err := db.Query("SELECT id, name, price FROM products")
-	if err != nil {
-		return nil, err
-	}
-
-	var products []Product
-
-	for rows.Next() {
-		var p Product
-		err := rows.Scan(&p.ID, &p.Name, &p.Price)
-		if err != nil {
-			return nil, err
-		}
-		products = append(products, p)
-	}
-	if err = rows.Err(); err != nil {
-		return nil, err
-	}
-	return products, nil
-}
-
-func updateProduct(id int, product *Product) (Product, error) {
-	var p Product
-	row := db.QueryRow(
-		"UPDATE public.products SET name=$1, price=$2 WHERE id=$3 RETURNING id, name, price;",
-		product.Name,
-		product.Price,
-		id,
-	)
-	err := row.Scan(&p.ID, &p.Name, &p.Price)
-	if err != nil {
-		return Product{}, err
-	}
-	return p, err
-}
-
-func deleteProduct(id int) error {
-	_, err := db.Exec(
-		"DELETE FROM products WHERE id=$1;",
-		id,
-	)
-	return err
 }
